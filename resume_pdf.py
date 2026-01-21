95fb70cc451c1c5cad77d47563bc29050ae66b9a
Raaif <raaif.m@pacewisdom.com>
2026-01-21 11:41:59 +0530

    Resume view changes

--- resume_pdf.py
+++ resume_pdf.py
@@ -5,47 +5,54 @@ def create_resume(employee, skills, projects):
     file_name = "resume.pdf"
     pdf = canvas.Canvas(file_name, pagesize=A4)
 
+    width, height = A4
     x = 40
     y = 800
 
+    pdf.setFont("Times-Bold", 20)
+    pdf.drawCentredString(width/2, y, "Curriculum Vitae")
+    y -= 20
+
+    pdf.line(x, y, width - x, y) 
+    y -= 30
+
     pdf.setFont("Helvetica-Bold", 16)
     pdf.drawString(x, y, employee.name)
-
     y -= 20
+    
     pdf.setFont("Helvetica", 10)
     pdf.drawString(x, y, f"{employee.email} | {employee.phone}")
-
     y -= 30
+
     pdf.setFont("Helvetica-Bold", 12)
     pdf.drawString(x, y, "Summary")
+    y -= 20
 
-    y -= 15
     pdf.setFont("Helvetica", 10)
     pdf.drawString(x, y, employee.summary)
-
     y -= 30
+
     pdf.setFont("Helvetica-Bold", 12)
     pdf.drawString(x, y, "Technical Skills")
+    y -= 20
 
-    y -= 15
     pdf.setFont("Helvetica", 10)
     for skill in skills:
         pdf.drawString(x, y, f"- {skill.skill}")
-        y -= 15
+        y -= 20
+    y -= 10
 
-    y -= 15
     pdf.setFont("Helvetica-Bold", 12)
     pdf.drawString(x, y, "Projects")
+    y -= 20
 
-    y -= 15
     pdf.setFont("Helvetica", 10)
     for project in projects:
         pdf.drawString(x, y, project.project_name)
-        y -= 15
-        pdf.drawString(x + 20, y, f"Role: {project.role}")
-        y -= 15
-        pdf.drawString(x + 20, y, project.description)
-        y -= 25
-
+        y -= 20
+        pdf.drawString(x + 25, y, f"Role: {project.role}")
+        y -= 20
+        pdf.drawString(x + 25, y, project.description)
+        y -= 30
     pdf.save()
     return file_name
