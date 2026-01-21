68a250fa619dd844965aac5d777e237528803369
Raaif <raaif.m@pacewisdom.com>
2026-01-21 12:26:16 +0530

    Changes in resume template

--- resume_pdf.py
+++ resume_pdf.py
@@ -1,5 +1,6 @@
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
+from textwrap import wrap
 
 def create_resume(employee, skills, projects):
     file_name = "resume.pdf"
@@ -21,16 +22,35 @@ def create_resume(employee, skills, projects):
     y -= 20
     
     pdf.setFont("Helvetica", 10)
-    pdf.drawString(x, y, f"{employee.email} | {employee.phone}")
+    pdf.drawString(x + 5, y, employee.email)
+    y -= 15
+    pdf.drawString(x + 5, y, employee.phone)
     y -= 30
 
     pdf.setFont("Helvetica-Bold", 12)
-    pdf.drawString(x, y, "Summary")
+    pdf.drawString(x, y, "Personal Information")
     y -= 20
 
     pdf.setFont("Helvetica", 10)
-    pdf.drawString(x, y, employee.summary)
+    pdf.drawString(x + 5, y, f"D.O.B: ")
+    y -= 15
+    pdf.drawString(x + 5, y, "Address: ")
+    y -= 15
+    pdf.drawString(x + 5, y, "Gender: ")
     y -= 30
+
+    pdf.setFont("Helvetica-Bold", 12)
+    pdf.drawString(x, y, "Career Objective")
+    y -= 20
+
+    pdf.setFont("Helvetica", 10)
+    max_chars = 110
+    lines = wrap(employee.summary, max_chars)
+
+    for line in lines:
+        pdf.drawString(x + 5, y, line)
+        y -= 14
+    y -= 16
 
     pdf.setFont("Helvetica-Bold", 12)
     pdf.drawString(x, y, "Technical Skills")
@@ -38,7 +58,7 @@ def create_resume(employee, skills, projects):
 
     pdf.setFont("Helvetica", 10)
     for skill in skills:
-        pdf.drawString(x, y, f"- {skill.skill}")
+        pdf.drawString(x + 5, y, f"- {skill.skill}")
         y -= 20
     y -= 10
 
@@ -48,11 +68,18 @@ def create_resume(employee, skills, projects):
 
     pdf.setFont("Helvetica", 10)
     for project in projects:
-        pdf.drawString(x, y, project.project_name)
+        pdf.drawString(x + 5, y, project.project_name)
         y -= 20
         pdf.drawString(x + 25, y, f"Role: {project.role}")
         y -= 20
-        pdf.drawString(x + 25, y, project.description)
-        y -= 30
+
+        max_chars = 110
+        lines = wrap(project.description, max_chars)
+
+        for line in lines:
+            pdf.drawString(x + 25, y, line)
+            y -= 14
+        y -= 16
+
     pdf.save()
     return file_name
